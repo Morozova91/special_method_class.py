73b7692b10503b3ec7ab97520777c5d1512d866a
Юлия <118264321+Morozova91@users.noreply.github.com>
2024-06-23 17:48:41 +0300

    Add files via upload

---
+++ special_method_class.py
@@ -0,0 +1,10 @@
+class House:
+    def __init__(self):
+        self.number_of_floors = 0
+
+    def set_new_number_of_floors(self, floors):
+        self.number_of_floors = floors
+        print(f"Количество этажей в доме: {self.number_of_floors}")
+
+
+
